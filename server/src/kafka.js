0a08e51ce4b106e13d4726cb8d9bc4e913b2531d
poorab patel <poorabpatel202@gmail.com>
2025-11-13 16:03:39 +0530

    set up production server

--- server/src/kafka.js
+++ server/src/kafka.js
@@ -1,12 +1,29 @@
-import { Kafka } from "kafkajs";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 dotenv.config();
+import { Kafka } from "kafkajs";
 
-const kafka = new Kafka({
-  clientId: process.env.KAFKA_CLIENT_ID,
-  brokers: [process.env.KAFKA_BROKER],
-});
+
+const brokers = process.env.KAFKA_BROKERS
+  ? process.env.KAFKA_BROKERS.split(",")
+  : ["localhost:9092"];
+
+
+const kafkaConfig = {
+  clientId: process.env.KAFKA_CLIENT_ID || "lotwise-backend",
+  brokers,
+};
+
+if (process.env.KAFKA_SASL_USERNAME && process.env.KAFKA_SASL_PASSWORD) {
+  kafkaConfig.ssl = true;
+  kafkaConfig.sasl = {
+    mechanism: "plain",
+    username: process.env.KAFKA_SASL_USERNAME,
+    password: process.env.KAFKA_SASL_PASSWORD,
+  };
+}
+
+const kafka = new Kafka(kafkaConfig);
 
 export const producer = kafka.producer();
-
-export const createConsumer = (groupId) => kafka.consumer({ groupId });
+export const createConsumer = (groupId) =>
+  kafka.consumer({ groupId });
