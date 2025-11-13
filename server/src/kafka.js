1f9b5e109af0ac3bb7f95884a48ae9d5270df080
poorab patel <poorabpatel202@gmail.com>
2025-11-13 16:27:27 +0530

    fixed kafka config

--- server/src/kafka.js
+++ server/src/kafka.js
@@ -2,11 +2,9 @@ import dotenv from "dotenv";
 dotenv.config();
 import { Kafka } from "kafkajs";
 
-
 const brokers = process.env.KAFKA_BROKERS
   ? process.env.KAFKA_BROKERS.split(",")
   : ["localhost:9092"];
-
 
 const kafkaConfig = {
   clientId: process.env.KAFKA_CLIENT_ID || "lotwise-backend",
@@ -16,7 +14,7 @@ const kafkaConfig = {
 if (process.env.KAFKA_SASL_USERNAME && process.env.KAFKA_SASL_PASSWORD) {
   kafkaConfig.ssl = true;
   kafkaConfig.sasl = {
-    mechanism: "plain",
+    mechanism: "scram-sha-256",     
     username: process.env.KAFKA_SASL_USERNAME,
     password: process.env.KAFKA_SASL_PASSWORD,
   };
